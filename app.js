6a8b2e228ee12a819f60fe495545a5f0ab0bbbe2
Nari Roh <nari.rohh@gmail.com>
2018-08-04 17:40:26 -0500

    Encoding user input

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 const request = require("request");
-/* 
-  request({}, (error, response, body));
-  To make HTTP request
-  takes 2 args,
-  1. Options object
-    Where you specify the things unique to your request: URL
-  2. Callback function 
-    It gets called once the data comes back from the HTTP endpoint (once that json data comes back into the node application)  
-    - body: is the core data that comes back from the server
-    - response: stores information about the response and about their request (statusCode)
-    - error: errors on the requested server
-*/
-request({
-  url: "http://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia",
-  json: true 
-  // 👆 Tells request the data coming back is JSON and it will take the JSON string and convert it to an object.
-}, (error, response, body) => {
-  // console.log(JSON.stringify(error, undefined, 2));
-  // the 3rd argument here, is to specify how many spaces you wanna use per indentation.
-  const { lat, lng } = body.results[0].geometry.location;
-  console.log(`Address: ${body.results[0].formatted_address}`);
+const yargs = require('yargs');
 
+const argv = yargs
+  .options({
+    a: {
+      demand: true, 
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+      // tells yargs to always parse the a or address's argument as a string
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv; 
+  // takes all of this configuration runs it through our arguments and stores the result in the argv variable
+
+  // console.log(argv);
+  var encodedAddress = encodeURIComponent(argv.address);
+
+request({
+  url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+  json: true 
+}, (error, response, body) => {
+  const { lat, lng } = body.results[0].geometry.location;
+  
+  console.log(`Address: ${body.results[0].formatted_address}`);
   console.log(`Latitude: ${lat}`);
   console.log(`Longitude: ${lng}`);
-
 });
